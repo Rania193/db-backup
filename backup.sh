83a13f91aa675e6a2da462e0190bcb7c92fb2368
Rania193 <rania.fahmy222@gmail.com>
2025-02-01 17:24:25 +0200

    removed host

--- backup.sh
+++ backup.sh
@@ -15,7 +15,7 @@ if [ ! -d "$BACKUP_DIR" ]; then
 fi
 
 # Perform the backup
-mysqldump -h "$DB_HOST" -u "$DB_USER" "$DB_NAME" > "$BACKUP_DIR/${DB_NAME}_backup_$TIMESTAMP.sql"
+mysqldump  -u "$DB_USER" "$DB_NAME" > "$BACKUP_DIR/${DB_NAME}_backup_$TIMESTAMP.sql"
 
 # Check if the backup was successful
 if [ $? -eq 0 ]; then
